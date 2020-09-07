34dd99196e3ca493fc95c46c6dc0f978019b2557
Jose <A01023661@itesm.mx>
2020-09-07 12:18:02 -0500

    HW

--- SortingMethods/main.cpp
+++ SortingMethods/main.cpp
@@ -16,25 +16,25 @@ for(p=0; p < sort1.elementos.size(); p++)
 
 std::cout << std::endl << "Sorters: " << std::endl;
 
-std::cout << "Sorting by Selection Sort " << std::endl;
+std::cout << "Sorting by Selection Sort O(n^2) " << std::endl;
 DECLARE_TIMING(t);
 START_TIMING(t);
 sort1.selectionSort(sort1.elementos);
 STOP_TIMING(t);
 SHOW_TIMING(t, "Selection Sort:");
-std::cout << std::endl << "Sorting by Bubble Sort " << std::endl;
+std::cout << std::endl << "Sorting by Bubble Sort Sort O(n^2) " << std::endl;
 DECLARE_TIMING(t1);
 START_TIMING(t1);
 sort1.bubbleSort(sort1.elementos);
 STOP_TIMING(t1);
 SHOW_TIMING(t1, "Bubble Sort:");
-std::cout << std::endl << " Sorting by Insertion Sort " << std::endl;
+std::cout << std::endl << " Sorting by Insertion Sort Sort O(n^2)" << std::endl;
 DECLARE_TIMING(t3);
 START_TIMING(t3);
 sort1.insertionSort(sort1.elementos);
 STOP_TIMING(t3);
 SHOW_TIMING(t3, "Insertion Sort:");
-std::cout << std::endl << "Sorting by Merge Sort " << std::endl;
+std::cout << std::endl << "Sorting by Merge Sort Sort O(nlogn) " << std::endl;
 DECLARE_TIMING(t4);
 START_TIMING(t4);
 sort1.mergeSort(sort1.elementos,0,sort1.elementos.size()-1);
